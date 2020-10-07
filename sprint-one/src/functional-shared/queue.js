524d0ad1c90018dade20ff0d1276945c6e299ccb
Lillian Eng <lillian.eng88@gmail.com>
2020-10-07 16:14:21 -0700

    finished the stack and queue functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,49 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // created a someInstant Object to store key values of the storage; front of the line; the back of the line
+  // used the extend method to add the queue methods
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.back = 0;
+  someInstance.front = 0;
+  extend(someInstance, queueMethods);
+  return someInstance;
+
+
+};
+// created an extend method to combine someInstance with queue methods
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+// create object of queue methods
+var queueMethods = {};
+// enqueue should assign a value to storage with the back key value
+// and return added value
+queueMethods.enqueue = function(value) {
+  this.storage[this.back] = value;
+  this.back++;
+  return value;
+};
+// dequeue should delete from the front of the queue and move the front value up by one
+// return deleted value
+queueMethods.dequeue = function() {
+  deletedVal = this.storage[this.front];
+  delete this.storage[this.front];
+  this.front++;
+  return deletedVal;
+};
+// count the current storage size
+queueMethods.size = function () {
+  var counter = 0;
+  for (let key in this.storage) {
+    counter++;
+  }
+  return counter;
 };
 
-var queueMethods = {};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,46 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // created a someInstant Object to store key values of the storage; top of the stack
+  // used the extend method to add the queue methods
+  var someInstance = {};
+  someInstance.storage = {};
+  someInstance.top = 0;
+
+  extend(someInstance, stackMethods);
+  return someInstance;
+
+};
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
-
+// push function to assign a value to the key of the current top
+// which is 1 more than the previous top
+// and return pushed value
+stackMethods.push = function(value) {
+  this.top++;
+  this.storage[this.top] = value;
+  return value;
+};
+// deleted the current top of the stack
+// update the current top key but subtracting one
+// return deleted value
+stackMethods.pop = function() {
+  var deletedValue = this.storage[this.top];
+  delete this.storage[this.top];
+  this.top--;
+  return deletedValue;
+};
+// count the current storage size
+stackMethods.size = function() {
+  var counter = 0;
+  for (key in this.storage) {
+    counter++;
+  }
+  return counter;
+};
 
