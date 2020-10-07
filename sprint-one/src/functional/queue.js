b7dcb34bf801c8cdd3391803a016add2a16e9858
Lillian Eng <lillian.eng88@gmail.com>
2020-10-07 13:04:56 -0700

    Finished the sprint for Stack and Queue function

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,29 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var front = 0;
+  var back = 0;
+
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[back] = value;
+    back++;
   };
 
   someInstance.dequeue = function() {
+    let deletedVal = storage[front];
+    delete storage[front];
+    front++;
+    return deletedVal;
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    for (let key in storage) {
+      counter++;
+    }
+    return counter;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -4,14 +4,32 @@ var Stack = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var storageSize = 0;
+  for (let key in storage) {
+    storageSize++;
+  }
+
+
   // Implement the methods below
   someInstance.push = function(value) {
+    storageSize++;
+    storage[storageSize] = value;
+    return storage[storageSize];
   };
 
   someInstance.pop = function() {
+    var deletedInstance = storage[storageSize];
+    delete storage[storageSize];
+    storageSize--;
+    return deletedInstance;
   };
 
   someInstance.size = function() {
+    var counter = 0;
+    for (let key in storage) {
+      counter++;
+    }
+    return counter;
   };
 
   return someInstance;
