e833eb6103b610c65098d0d2855400aa96d9709a
Lillian Eng <lillian.eng88@gmail.com>
2020-10-07 17:46:05 -0700

    Finished the pseudoclassical iteration of stack and queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // for pseudoclassical you just need dont to declare object
+  // then assign a prototype chain to the methods
+  this.storage = {};
+  this.front = 0;
+  this.back = 0;
 };
 
+// assign the Queue to the methods with prototype chain
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.back] = value;
+  this.back++;
+  return value;
+};
+
+Queue.prototype.dequeue = function() {
+  deletedVal = this.storage[this.front];
+  delete this.storage[this.front];
+  this.front++;
+  return deletedVal;
+};
+
+Queue.prototype.size = function () {
+  var counter = 0;
+  for (let key in this.storage) {
+    counter++;
+  }
+  return counter;
+};
+// create a new class
+var someInstance = new Queue;
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  this.top = 0;
+  this.storage = {};
 };
+// implemented the Stack pseudoclassical with prototype chain
+Stack.prototype.push = function(value) {
+  this.top++;
+  this.storage[this.top] = value;
+  return value;
+};
+
+Stack.prototype.pop = function() {
+  var deletedValue = this.storage[this.top];
+  delete this.storage[this.top];
+  this.top--;
+  return deletedValue;
+};
+
+Stack.prototype.size = function() {
+  var counter = 0;
+  for (key in this.storage) {
+    counter++;
+  }
+  return counter;
+};
+//declared a new class someinstance
+var someInstance = new Stack;
 
 
