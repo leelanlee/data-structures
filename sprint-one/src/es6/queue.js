d9c3ca7928f1c4ae68f4c0018857cefdb81fcdde
Lillian Eng <lillian.eng88@gmail.com>
2020-10-07 17:46:47 -0700

    Finished the es6 iteration of stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,30 @@
 class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //create constructor with starting values
   constructor() {
+    this.storage = {};
+    this.front = 0;
+    this.back = 0;
   }
-
+  enqueue(value) {
+    this.storage[this.back] = value;
+    this.back++;
+    return value;
+  }
+  dequeue() {
+    var deletedVal = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+    return deletedVal;
+  }
+  size() {
+    var counter = 0;
+    for (let key in this.storage) {
+      counter++;
+    }
+    return counter;
+  }
 }
+//declared new class some instances
+var someInstances = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.top = 0;
   }
-
+  push(value) {
+    this.top++;
+    this.storage[this.top] = value;
+    return value;
+  }
+  pop() {
+    var deletedVal = this.storage[this.top];
+    delete this.storage[this.top];
+    this.top--;
+    return deletedVal;
+  }
+  size() {
+    var counter = 0;
+    for (let key in this.storage) {
+      counter++;
+    }
+    return counter;
+  }
 }
+var someInstance = new Stack();
