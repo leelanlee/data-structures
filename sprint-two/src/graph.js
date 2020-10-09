7c064a8543db6f1f4fde911fd7017d0bfb0ee5b7
Lillian Eng <lillian.eng88@gmail.com>
2020-10-08 20:53:09 -0700

    implemented addNode, contains, removeNode, hasEdge, addEdge

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,6 +1,7 @@
+
+
 // Instantiate a new graph
 var Graph = function(val) {
-
 };
 
 // Add a node to the graph, passing in the node's value.
@@ -9,63 +10,69 @@ Graph.prototype.addNode = function(node) {
   this[node] = [];
 };
 
-
-
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  //  iterate thru each property of the node
-  for (let key in this) {
-      if(Number(key) === node) { return true; }
+  for (var key in this) {
+    if (+key === node) { return true; }
   }
-    return false;
+
+  return false;
 };
+
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  for (let key in this) {
-
-    if (Number(key) === node) {
-      delete this[key];
-    }
+// iterate thru Graph
+  for (var key in this) {
+  //    if a key equals node
+    if (+key === node) { delete this[key]; }
+  //      delete that property
   }
 
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-
-  if (this[fromNode] && this[toNode]) {
-    if (this[fromNode].indexOf(toNode) !== -1 && this[toNode].indexOf(fromNode) !== -1) {
-      return true;
-    }
-
-  }
-  return false;
+  //  if both nodes contain each other
+  var from = this[fromNode].indexOf(toNode) !== -1;
+  var to = this[toNode].indexOf(fromNode) !== -1;
+  return from && to;
+  //    return true
+  //  otherwises return false
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-  if(this.contains(fromNode) && this.contains(toNode)) {
-      if(this[fromNode].indexOf(toNode) === -1) {
-       this[fromNode].push(toNode);
-      }
-      if(this[toNode].indexOf(fromNode) === -1) {
-       this[toNode].push(fromNode);
-      }
-}
+  //  if one node doesn't exist, end function
+  if (!this.contains(fromNode) || !this.contains(toNode)) { return null; }
+  //  push the fromNode into the toNode
+  this[fromNode].push(toNode);
+  //  push the toNode into the fromNode
+  this[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-    this[fromNode].splice(this[toNode].indexOf(toNode), 1);
-    this[toNode].splice(this[fromNode].indexOf(fromNode), 1);
+//
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-  for (let node in this) {
-    cb(Number(node));
-  }
 };
 
+/*
+ * Complexity: What is the time complexity of the above functions?
+ *
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
+
+
+
+/*
+ * Complexity: What is the time complexity of the above functions?
+ */
 
 
